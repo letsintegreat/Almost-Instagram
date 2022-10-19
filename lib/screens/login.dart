623ed1d2d0bb72ce504bb2dff461aa13ed6d0df0
Harshit Seksaria <37345795+letsintegreat@users.noreply.github.com>
2022-10-20 02:32:01 +0530

    Fix scroll error in auth pages

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -48,162 +48,170 @@ class _LoginScreenState extends State<LoginScreen> {
           );
         }),
       ),
-      body: Column(
-        children: [
-          Spacer(),
-          Center(
-            child: Image(
-              image: AssetImage('assets/logo.png'),
-              height: 50.0,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 90.0,
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 40.0),
-            child: TextField(
-              controller: _email,
-              style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xFF121212),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                hintText: 'Email',
-                hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
+            Center(
+              child: Image(
+                image: AssetImage('assets/logo.png'),
+                height: 50.0,
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
-            child: TextField(
-              controller: _password,
-              obscureText: true,
-              style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xFF121212),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
+            Padding(
+              padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 40.0),
+              child: TextField(
+                controller: _email,
+                style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFF121212),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  hintText: 'Email',
+                  hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                hintText: 'Password',
-                hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
               ),
             ),
-          ),
-          Row(
-            children: [
-              Spacer(),
-              Padding(
-                padding:
-                    const EdgeInsets.only(right: 16.0, top: 20.0, bottom: 30.0),
-                child: Text(
-                  "Forgot password?",
-                  textAlign: TextAlign.right,
-                  style: TextStyle(fontSize: 12.0, color: Color(0xFF3797EF)),
+            Padding(
+              padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
+              child: TextField(
+                controller: _password,
+                obscureText: true,
+                style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFF121212),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  hintText: 'Password',
+                  hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
                 ),
               ),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: SizedBox(
-              width: double.infinity,
-              height: 44.0,
-              child: Builder(builder: (context) {
-                return ElevatedButton(
-                  onPressed: () => login(context),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
+            ),
+            Row(
+              children: [
+                Spacer(),
+                Padding(
+                  padding:
+                      const EdgeInsets.only(right: 16.0, top: 20.0, bottom: 30.0),
+                  child: Text(
+                    "Forgot password?",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(fontSize: 12.0, color: Color(0xFF3797EF)),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 44.0,
+                child: Builder(builder: (context) {
+                  return ElevatedButton(
+                    onPressed: () => login(context),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Log in',
+                      style: TextStyle(fontSize: 14.0, fontFamily: 'Roboto'),
+                    ),
+                  );
+                }),
+              ),
+            ),
+            Container(
+              height: 90.0,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      height: 0.5,
+                      color: Color(0x99FFFFFF),
                     ),
                   ),
+                ),
+                Text("OR",
+                    style: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF))),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      height: 0.5,
+                      color: Color(0x99FFFFFF),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0, right: 5.0),
                   child: Text(
-                    'Log in',
-                    style: TextStyle(fontSize: 14.0, fontFamily: 'Roboto'),
+                    "Don't have an account? ",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(fontSize: 12.0, color: Color(0x99FFFFFF)),
                   ),
-                );
-              }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: Text(
+                    "Sign up.",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(fontSize: 12.0, color: Color(0xFF3797EF)),
+                  ),
+                ),
+                Spacer(),
+              ],
             ),
-          ),
-          Spacer(),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Container(
-                    height: 0.5,
-                    color: Color(0x99FFFFFF),
-                  ),
-                ),
-              ),
-              Text("OR",
-                  style: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF))),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Container(
-                    height: 0.5,
-                    color: Color(0x99FFFFFF),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0, right: 5.0),
-                child: Text(
-                  "Don't have an account? ",
-                  textAlign: TextAlign.right,
-                  style: TextStyle(fontSize: 12.0, color: Color(0x99FFFFFF)),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0),
-                child: Text(
-                  "Sign up.",
-                  textAlign: TextAlign.right,
-                  style: TextStyle(fontSize: 12.0, color: Color(0xFF3797EF)),
-                ),
-              ),
-              Spacer(),
-            ],
-          ),
-          Spacer(),
-          Container(
-            height: 0.5,
-            color: Color(0x99FFFFFF),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-            child: Text(
-              "Instagram from Meta",
-              textAlign: TextAlign.right,
-              style: TextStyle(fontSize: 12.0, color: Color(0x99FFFFFF)),
+            Container(
+              height: 90.0,
             ),
-          ),
-        ],
+            Container(
+              height: 0.5,
+              color: Color(0x99FFFFFF),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Text(
+                "Instagram from Meta",
+                textAlign: TextAlign.right,
+                style: TextStyle(fontSize: 12.0, color: Color(0x99FFFFFF)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -79,199 +79,207 @@ class _SignupScreenState extends State<SignupScreen> {
           );
         }),
       ),
-      body: Column(
-        children: [
-          Spacer(),
-          Center(
-            child: Image(
-              image: AssetImage('assets/logo.png'),
-              height: 50.0,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 60.0,
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 40.0),
-            child: TextField(
-              controller: _username,
-              style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xFF121212),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                hintText: 'Username',
-                hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
+            Center(
+              child: Image(
+                image: AssetImage('assets/logo.png'),
+                height: 50.0,
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
-            child: TextField(
-              controller: _password,
-              obscureText: true,
-              style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xFF121212),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
+            Padding(
+              padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 40.0),
+              child: TextField(
+                controller: _username,
+                style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFF121212),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  hintText: 'Username',
+                  hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                hintText: 'Password',
-                hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
-            child: TextField(
-              controller: _confirm,
-              obscureText: true,
-              style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xFF121212),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
+            Padding(
+              padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
+              child: TextField(
+                controller: _password,
+                obscureText: true,
+                style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFF121212),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  hintText: 'Password',
+                  hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                hintText: 'Confirm Password',
-                hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
-            child: TextField(
-              controller: _email,
-              style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Color(0xFF121212),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
+            Padding(
+              padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
+              child: TextField(
+                controller: _confirm,
+                obscureText: true,
+                style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFF121212),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  hintText: 'Confirm Password',
+                  hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                hintText: 'Email',
-                hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
-            child: SizedBox(
-              width: double.infinity,
-              height: 44.0,
-              child: Builder(builder: (context) {
-                return ElevatedButton(
-                  onPressed: () => signup(context),
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
+            Padding(
+              padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
+              child: TextField(
+                controller: _email,
+                style: TextStyle(fontSize: 14.0, color: Color(0xFFFFFFFF)),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFF121212),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0x33FFFFFF), width: 1),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  hintText: 'Email',
+                  hintStyle: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF)),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 12.0),
+              child: SizedBox(
+                width: double.infinity,
+                height: 44.0,
+                child: Builder(builder: (context) {
+                  return ElevatedButton(
+                    onPressed: () => signup(context),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                    ),
+                    child: Text(
+                      'Sign up',
+                      style: TextStyle(fontSize: 14.0, fontFamily: 'Roboto'),
+                    ),
+                  );
+                }),
+              ),
+            ),
+            Container(
+              height: 60.0,
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      height: 0.5,
+                      color: Color(0x99FFFFFF),
                     ),
                   ),
+                ),
+                Text("OR",
+                    style: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF))),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Container(
+                      height: 0.5,
+                      color: Color(0x99FFFFFF),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Spacer(),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0, right: 5.0),
                   child: Text(
-                    'Sign up',
-                    style: TextStyle(fontSize: 14.0, fontFamily: 'Roboto'),
+                    "Have an account? ",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(fontSize: 12.0, color: Color(0x99FFFFFF)),
                   ),
-                );
-              }),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40.0),
+                  child: Text(
+                    "Log in.",
+                    textAlign: TextAlign.right,
+                    style: TextStyle(fontSize: 12.0, color: Color(0xFF3797EF)),
+                  ),
+                ),
+                Spacer(),
+              ],
             ),
-          ),
-          Spacer(),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Container(
-                    height: 0.5,
-                    color: Color(0x99FFFFFF),
-                  ),
-                ),
-              ),
-              Text("OR",
-                  style: TextStyle(fontSize: 14.0, color: Color(0x99FFFFFF))),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Container(
-                    height: 0.5,
-                    color: Color(0x99FFFFFF),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Spacer(),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0, right: 5.0),
-                child: Text(
-                  "Have an account? ",
-                  textAlign: TextAlign.right,
-                  style: TextStyle(fontSize: 12.0, color: Color(0x99FFFFFF)),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 40.0),
-                child: Text(
-                  "Log in.",
-                  textAlign: TextAlign.right,
-                  style: TextStyle(fontSize: 12.0, color: Color(0xFF3797EF)),
-                ),
-              ),
-              Spacer(),
-            ],
-          ),
-          Spacer(),
-          Container(
-            height: 0.5,
-            color: Color(0x99FFFFFF),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-            child: Text(
-              "Instagram from Meta",
-              textAlign: TextAlign.right,
-              style: TextStyle(fontSize: 12.0, color: Color(0x99FFFFFF)),
+            Container(
+              height: 60.0,
             ),
-          ),
-        ],
+            Container(
+              height: 0.5,
+              color: Color(0x99FFFFFF),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Text(
+                "Instagram from Meta",
+                textAlign: TextAlign.right,
+                style: TextStyle(fontSize: 12.0, color: Color(0x99FFFFFF)),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
